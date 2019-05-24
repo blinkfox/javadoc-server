1a6d7a45bd004be3101a3e5862356f323d9fd5cb
blinkfox <chenjiayin1990@163.com>
2019-05-25 00:52:38 +0800

    修改 解压缩的相关功能

--- src/main/java/com/blinkfox/javadoc/service/JarService.java
+++ src/main/java/com/blinkfox/javadoc/service/JarService.java
@@ -5,11 +5,9 @@ import com.blinkfox.javadoc.entity.JarInfo;
 import com.blinkfox.javadoc.exception.RunException;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Optional;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import javax.annotation.Resource;
@@ -33,12 +31,21 @@ public class JarService {
     @Resource
     private SystemConfig systemConfig;
 
+    public void downloadAndDecompressJar(JarInfo jarInfo) {
+        Optional<File> jarFileOptional = this.download(jarInfo);
+        if (!jarFileOptional.isPresent()) {
+            throw new RunException("未下载到对应的 jar 包!");
+        }
+
+        this.decompressJar("/Users/blinkfox/Downloads/test", jarFileOptional.get());
+    }
+
     /**
      * 根据 jar 包的信息下载对应的 jar 包.
      *
      * @param jarInfo jar 包的信息
      */
-    public void download(JarInfo jarInfo) {
+    public Optional<File> download(JarInfo jarInfo) {
         if (jarInfo == null || jarInfo.valid()) {
             throw new RunException("需要下载的 jar 包相关信息不全！");
         }
@@ -52,7 +59,7 @@ public class JarService {
         // 循环遍历下载各个 Maven 仓库的 javadoc jar 包，直到下载到为止.
         for (String repo : mvnRepos) {
             String url = jarInfo.joinJavadocDownloadUrl(repo);
-             File jarFile = new File(FileUtils.getTempDirectoryPath() + File.separator + jarInfo.getJavadocJarName());
+            File jarFile = new File(FileUtils.getTempDirectoryPath() + File.separator + jarInfo.getJavadocJarName());
             log.info("\n--- 需要下载的url地址为:{}\n--- 生成的 javadoc 文件路径为: {}", url, jarFile);
 
             // 下载 javadoc.jar 文件，并写入到文件中.
@@ -68,41 +75,37 @@ public class JarService {
 
             // 如果 javadoc.jar 文件已经存在，就跳出循环.
             if (jarFile.exists()) {
-                log.info("该文件已经存在，将跳出循环，不再下载 jar 包了.");
-                jarInfo.setJarFile(jarFile);
-                log.info("下载 jar 包完成，耗时: {} ms.", System.currentTimeMillis() - start);
-                break;
+                log.info("已下载到 jar 包，耗时: {} ms.", System.currentTimeMillis() - start);
+                return Optional.of(jarFile);
             }
         }
-        log.info("执行完毕!");
+
+        return Optional.empty();
     }
 
     /**
      * 解压缩 jar 文件.
      *
-     * @param jarInfo jar信息
+     * @param destDir 解压的目标目录
+     * @param jarFile jar文件
      */
-    public void decompressJar(JarInfo jarInfo) throws IOException {
-        String destDir = "";
-        JarFile jar = new JarFile(jarInfo.getJarFile());
-        Enumeration enumEntries = jar.entries();
-        while (enumEntries.hasMoreElements()) {
-            JarEntry file = (java.util.jar.JarEntry) enumEntries.nextElement();
-            java.io.File f = new java.io.File(destDir + java.io.File.separator + file.getName());
-            if (file.isDirectory()) {
-                f.mkdir();
-                continue;
-            }
+    private void decompressJar(String destDir, File jarFile) {
+        try (JarFile jar = new JarFile(jarFile)) {
+            Enumeration enumEntries = jar.entries();
+            while (enumEntries.hasMoreElements()) {
+                JarEntry jarEntry = (JarEntry) enumEntries.nextElement();
+                File file = new File(destDir + File.separator + jarEntry.getName());
+                if (jarEntry.isDirectory()) {
+                    FileUtils.forceMkdir(file);
+                    continue;
+                }
 
-            InputStream is = jar.getInputStream(file);
-            java.io.FileOutputStream fos = new FileOutputStream(f);
-            while (is.available() > 0) {
-                fos.write(is.read());
+                FileUtils.copyInputStreamToFile(jar.getInputStream(jarEntry), file);
             }
-            fos.close();
-            is.close();
+        } catch (Exception e) {
+            log.error("解压缩 jar 文件出错！", e);
+            throw new RunException("解压缩 jar 文件出错!", e);
         }
-        jar.close();
     }
 
 }
